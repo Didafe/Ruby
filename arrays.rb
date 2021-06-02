33adfcf3c106a1187cd01d56b9c8632c0246b4e9
Matrix <dida_fernando#hotmail.com>
2021-06-01 22:53:01 -0300

    arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,33 @@
+lista = [] 
+
+# Lita é um objeto do tipo array
+p lista.class
+# Tamanho
+p lista.length
+# Incluindo um valor
+p lista << 42
+
+p lista << 56
+
+p lista.length
+
+# Aceita qualquer tipo de dado
+p lista << "minha string"
+p lista << true
+
+p lista.length
+
+# Pegando o primeiro ítem da lista
+p lista.first
+
+# Pegando o último ítem da lista
+p lista.last
+
+# Verificando se a lista é vazia
+p lista.empty?
+
+# Juntando listas
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+p resusldado = a + b
